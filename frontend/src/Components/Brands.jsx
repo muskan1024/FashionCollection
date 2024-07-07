cc7a61716946aabc375e4db739ffdec23a4389c9
NazarShaikh <shaikhnazar265@gmail.com>
2024-07-07 19:59:30 +0530

    brands scroll

--- frontend/src/Components/Brands.jsx
+++ frontend/src/Components/Brands.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import shop from './shop.css'
 
 const Brands = () => {
+  const scrollContainerRef = useRef(null);
+
+  useEffect(() => {
+    const scrollContainer = scrollContainerRef.current;
+    let scrollAmount = 0;
+
+    function scrollStep() {
+      if (scrollAmount < scrollContainer.scrollWidth - scrollContainer.clientWidth) {
+        scrollContainer.scrollLeft += 1;
+        scrollAmount += 1;
+        requestAnimationFrame(scrollStep);
+      } else {
+        scrollContainer.scrollLeft = 0; // Reset scroll position to start
+        scrollAmount = 0;
+        requestAnimationFrame(scrollStep);
+      }
+    }
+
+    scrollStep();
+  }, []);
+
   return (
     <div className="mt-10">
       <div className="text-3xl text-cyan-900 font-mono font-bold tracking-widest">
@@ -15,8 +36,9 @@ const Brands = () => {
           width: "100%",
         }}
         className="h-72 mt-3 text-center overflow-x-scroll mb-6"
+        ref={scrollContainerRef}
       >
-        <div className="scroll-container">
+        <div className="flex">
           <img
             className="w-[100%] mx-3 object-cover"
             src="/images/RaymondLogo.jpg"
