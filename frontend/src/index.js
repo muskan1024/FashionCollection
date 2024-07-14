744f6200123e56f21251b9f4d62ee6bdd850696b
muskan1024 <shaikhmuskan1024@gmail.com>
2024-07-14 20:02:54 +0530

    vercel analytics

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,15 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import Store from "./redux/Store";
+import { Analytics } from "@vercel/analytics/react"
+import {SpeedInsights} from "@vercel/speed-insights/react"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={Store}>
+      <Analytics/>
+      <SpeedInsights/>
       <App />
     </Provider>
   </React.StrictMode>
